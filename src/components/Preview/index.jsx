19ea285bcabcbc33d91e0978f669ef7663e82e5b
shoonia <zaytsev126@gmail.com>
2019-09-19 19:21:48 +0300

    Copy to clipboard

--- src/components/Preview/index.jsx
+++ src/components/Preview/index.jsx
@@ -1,19 +1,35 @@
 import { h } from 'preact';
 import useStoreon from 'storeon/preact';
+import { useCallback, useRef } from 'preact/hooks';
 
 import { createConfig } from './fn';
 import st from './styles.css';
 
 function Preview() {
   const { items } = useStoreon('items');
+  const value = createConfig(items);
+  const area = useRef(null);
+
+  const clipboard = useCallback(() => {
+    area.current.select();
+    document.execCommand('copy');
+  }, []);
 
   return (
-    <textarea
-      className={st.out}
-      readOnly
-    >
-      {createConfig(items)}
-    </textarea>
+    <div className={st.box}>
+      <textarea
+        ref={area}
+        className={st.out}
+        readOnly
+        value={value} />
+      <button
+        type="button"
+        className={st.copy}
+        onClick={clipboard}
+      >
+        Copy to clipboard
+      </button>
+    </div>
   );
 }
 
