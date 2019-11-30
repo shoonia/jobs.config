f1146e3936adecd39d1907c6be88ce22f2475988
shoonia <alexanderz@wix.com>
2019-12-01 00:52:15 +0200

    Fix double dots

--- src/components/Tooltip/FunctionLocation/util.js
+++ src/components/Tooltip/FunctionLocation/util.js
@@ -5,11 +5,10 @@ function hasError(path, isLast) {
   const DOT_CHAR_CODE = 46;
 
   return path === ''
-   // eslint-disable-next-line no-useless-escape
-   || /[^\w\d\._-]/.test(path)
+   || /[^\w\d\._-]/.test(path) // eslint-disable-line no-useless-escape
    || path.charCodeAt(0) === DOT_CHAR_CODE
    || path.charCodeAt(path.length - 1) === DOT_CHAR_CODE
-   || (isLast && !/\.jsw?$/.test(path));
+   || (isLast && !/[^\.]\.jsw?$/.test(path)); // eslint-disable-line no-useless-escape
 }
 
 function iconClass(path, isLast) {
