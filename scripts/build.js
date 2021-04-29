b56f73f3416d67a20e2e4564d5037f350bbb0d16
shoonia <alexanderz@wix.com>
2021-04-29 16:18:00 +0300

    Clear config

--- scripts/build.js
+++ scripts/build.js
@@ -5,7 +5,6 @@ require('../config/env')(buildEnv);
 const webpack = require('webpack');
 const configFactory = require('../config/webpack.config');
 const paths = require('../config/paths');
-const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
 const { measureFileSizesBeforeBuild, printFileSizesAfterBuild } = require('react-dev-utils/FileSizeReporter');
 const printBuildError = require('react-dev-utils/printBuildError');
 
@@ -16,55 +15,28 @@ const WARN_AFTER_CHUNK_GZIP_SIZE = 1024 * 1024;
   try {
     const previousFileSizes = await measureFileSizesBeforeBuild(paths.appBuild);
 
-    const [stats, warnings] = await new Promise((resolve, reject) => {
+    const stats = await new Promise((resolve, reject) => {
       const config = configFactory(buildEnv);
 
-      console.log('\nProduction build...\n');
-
       webpack(config, (error, stats) => {
-        let messages;
-
-        if (error) {
-          if (!error.message) {
-            return reject(error);
-          }
-
-          let errMessage = error.message;
-
-          // Add additional information for postcss errors
-          if (Object.prototype.hasOwnProperty.call(error, 'postcssNode')) {
-            errMessage +=
-            '\nCompileError: Begins at CSS selector ' +
-            error['postcssNode'].selector;
-          }
-
-          messages = formatWebpackMessages({
-            errors: [errMessage],
-            warnings: [],
-          });
-        } else {
-          messages = formatWebpackMessages(
-            stats.toJson({ all: false, warnings: true, errors: true }),
-          );
-        }
-
-        if (messages.errors.length > 0) {
-          return reject(new Error(messages.errors.join('\n\n')));
-        }
-
-        resolve([stats, messages.warnings]);
+        if (error) reject(error);
+        else resolve(stats);
       });
     });
 
-    if (warnings.length > 0) {
-      console.log(
-        'Compiled with warnings.\n\n\n',
-        'Search for the keywords to learn more about each warning.',
-        'To ignore, add // eslint-disable-next-line to the line before.\n',
-      );
-    } else {
-      console.log('\nCompiled successfully.\n');
+    const { warnings, errors } = stats.toJson({
+      all: false,
+      warnings: true,
+      errors: true,
+    });
+
+    if (errors.length > 0) {
+      return Promise.reject(new Error(errors[0].message));
     }
+
+    warnings.forEach((warn) => {
+      console.log(warn.message, '\n');
+    });
 
     console.log('File sizes after gzip:\n');
 
