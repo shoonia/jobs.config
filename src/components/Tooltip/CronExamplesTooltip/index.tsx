113ae40b4b4d1d276fb1a102f3a48e8925eb28ad
shoonia <alexanderz@wix.com>
2021-07-09 00:46:36 +0300

    Add cron examples

--- src/components/Tooltip/CronExamplesTooltip/index.tsx
+++ src/components/Tooltip/CronExamplesTooltip/index.tsx
@@ -1,28 +1,61 @@
 import { FunctionComponent } from 'preact';
+import { useStoreon } from 'storeon/preact';
 
 import s from './styles.css';
+import { KEYS } from '../../../constants';
 import { Button } from '../../Button';
+import { TEvents, TState } from '../../../store';
 import { cronExamples } from '../../../util/cronExamples';
+import { classNames } from '../../../util/component';
 
 interface Props {
   target: HTMLElement;
 }
 
-export const CronExamplesTooltip: FunctionComponent<Props> = () => {
-  const list = cronExamples.map((i) => (
-    <li key={i.value}>
-      <Button
-        blank
-        value={i.value}
-        title={i.value}
+export const CronExamplesTooltip: FunctionComponent<Props> = ({ target }) => {
+  const { items, dispatch } = useStoreon<TState, TEvents>('items');
+
+  const { id } = target.dataset;
+  const { cronExpression } = items.find((i) => i.id === id) || {};
+
+  const onClick = (ev: MouseEvent): void => {
+    if (ev.target instanceof HTMLButtonElement) {
+      dispatch('items/update', {
+        id: id || '',
+        name: KEYS.cronExpression,
+        value: ev.target.value,
+      });
+    }
+  };
+
+  const list = cronExamples.map((i) => {
+    const buttonClassName = classNames([
+      s.btn,
+      cronExpression === i.value && s.active,
+    ]);
+
+    return (
+      <li
+        key={i.value}
+        className={s.item}
       >
-        {i.label}
-      </Button>
-    </li>
-  ));
+        <Button
+          blank
+          value={i.value}
+          title={i.value}
+          className={buttonClassName}
+        >
+          {i.label}
+        </Button>
+      </li>
+    );
+  });
 
   return (
-    <ul className={s.list}>
+    <ul
+      className={s.list}
+      onClick={onClick}
+    >
       {list}
     </ul>
   );
