4dd3105d8669bd9da7a32624f61193d0422c90b1
shoonia <alexanderz@wix.com>
2021-12-27 16:44:08 +0200

    Use type import

--- src/components/Parser/EditButton.tsx
+++ src/components/Parser/EditButton.tsx
@@ -2,7 +2,7 @@ import { useStoreon } from 'storeon/preact';
 
 import type { IState, IEvents } from '../../store';
 import { Button } from '../Button';
-import { IConfig, createItems } from '../../util/items';
+import { type IConfig, createItems } from '../../util/items';
 import { ROUTER } from '../../constants';
 
 interface Props {
--- src/hooks/formScope.ts
+++ src/hooks/formScope.ts
@@ -1,6 +1,6 @@
 import { createContext, useContext } from 'preact/compat';
 
-import { IItem, newItem } from '../util/items';
+import { type IItem, newItem } from '../util/items';
 
 export const FormScope = createContext<IItem>(newItem());
 
