1b7d9068e1ce293ffd2c2464c2d58a532e1be624
shoonia <alexanderz@wix.com>
2023-02-06 23:44:06 +0200

    Add memo

--- src/components/Jobs/Item.tsx
+++ src/components/Jobs/Item.tsx
@@ -1,3 +1,5 @@
+import { memo } from 'preact/compat';
+
 import s from './styles.css';
 import { Period } from './Period';
 import { ItemMenu } from './ItemMenu';
@@ -12,7 +14,7 @@ interface Props {
   isNew?: boolean;
 }
 
-export const Item: FC<Props> = ({ id, update, isNew }) => {
+export const Item: FC<Props> = memo(({ id, update, isNew }) => {
   useNewItem(id, isNew);
 
   return (
@@ -29,4 +31,4 @@ export const Item: FC<Props> = ({ id, update, isNew }) => {
       <ItemMenu />
     </form>
   );
-};
+});
