2a88c0ef9b0c0e63133db53dec918ce0e0921775
shoonia <alexanderz@wix.com>
2023-02-08 15:47:47 +0200

    Refactoring RegExp

--- src/util/validator.ts
+++ src/util/validator.ts
@@ -4,8 +4,8 @@ export type TValidator = (val: string) => boolean;
 
 const FUNCTION_NAME = /^[$a-z_][\da-z_$]*$/i;
 const UTC = /^([01]\d|2[0-3]):([0-5]\d)$/;
-const PATH = /[^\w\d.-]/;
-const FILE_NAME = /[^.]\.jsw?$/;
+const INVALID_PATH_CHARS = /[^\w\d.-]/;
+const FILE_EXT = /\.jsw?$/;
 
 const keywords = new Set([
   'break',
@@ -55,10 +55,10 @@ export const isUTCTime = (val: unknown): val is string => {
 
 export const isInvalidPath = (path: string, isLast: boolean): boolean => {
   return path === ''
-    || PATH.test(path)
+    || INVALID_PATH_CHARS.test(path)
     || path.startsWith('.')
     || path.endsWith('.')
-    || isLast && !FILE_NAME.test(path);
+    || isLast && !FILE_EXT.test(path);
 };
 
 const isValidPath = (path: string, index: number, list: string[]): boolean => {
