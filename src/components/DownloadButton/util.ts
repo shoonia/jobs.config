92630e8abe1febb6002a1b20652877fbd6676ead
shoonia <alexanderz@wix.com>
2023-12-02 16:22:25 +0200

    Add startIn directory

--- src/components/DownloadButton/util.ts
+++ src/components/DownloadButton/util.ts
@@ -3,6 +3,7 @@ export const isSupportFilePicker = typeof showSaveFilePicker === 'function';
 export const saveFile = async (content: string): Promise<void> => {
   const file = await showSaveFilePicker({
     suggestedName: 'jobs.config',
+    startIn: 'desktop',
   });
 
   const writable = await file.createWritable();
